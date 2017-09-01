d43fc007c7a4e2f90c55b2b388cfceb5ff7573d3
Martin d'Anjou <martin.danjou14@gmail.com>
2017-09-01 13:55:38 -0400

    Add jenkins-test-harness to javadoc
    * Add jenkins-test-harness
    * Order list in alphabetical order

--- scripts/generate-javadoc-components.groovy
+++ scripts/generate-javadoc-components.groovy
@@ -5,9 +5,10 @@ def keyComparator = [compare: { e1, e2 -> e1.name.compareToIgnoreCase(e2.name) }
 
 def components = new ArrayList<Artifact>();
 components.addAll(Arrays.asList(
+    new Artifact("GitHub API", "org.kohsuke", "github-api", null, "http://github-api.kohsuke.org/"),
+    new Artifact("Jenkins Test Harness", "org.jenkins-ci.main", "jenkins-test-harness", null, "https://github.com/jenkinsci/jenkins-test-harness"),
     new Artifact("Remoting", "org.jenkins-ci.main", "remoting", null, "https://github.com/jenkinsci/remoting"),
-    new Artifact("XStream", "org.jvnet.hudson", "xstream", null, "https://github.com/jenkinsci/xstream"),
-    new Artifact("GitHub API", "org.kohsuke", "github-api", null, "http://github-api.kohsuke.org/")
+    new Artifact("XStream", "org.jvnet.hudson", "xstream", null, "https://github.com/jenkinsci/xstream")
 ))
 
 
