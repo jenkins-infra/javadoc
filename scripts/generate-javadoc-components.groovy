1cd10d4b84810abf5575a8254f3abc5f86ecc65d
Basil Crow <me@basilcrow.com>
2022-01-13 07:53:35 -0800

    Add additional components

--- scripts/generate-javadoc-components.groovy
+++ scripts/generate-javadoc-components.groovy
@@ -17,7 +17,13 @@ components.addAll(Arrays.asList(
     new Artifact("Task Reactor Lib", "org.jenkins-ci", "task-reactor", null, "https://github.com/jenkinsci/lib-task-reactor"),
     new Artifact("Version Number Lib", "org.jenkins-ci", "version-number", null, "https://github.com/jenkinsci/lib-version-number"),
     new Artifact("Crypto Util Lib", "org.jenkins-ci", "crypto-util", null, "https://github.com/jenkinsci/lib-crypto-util"),
-    new Artifact("Annotation Indexer Lib", "org.jenkins-ci", "annotation-indexer", null, "https://github.com/jenkinsci/lib-annotation-indexer")
+    new Artifact("Annotation Indexer Lib", "org.jenkins-ci", "annotation-indexer", null, "https://github.com/jenkinsci/lib-annotation-indexer"),
+    new Artifact("Access Modifier Annotation", "org.jenkins-ci", "access-modifier-annotation", null, "https://github.com/jenkinsci/lib-access-modifier"),
+    new Artifact("Access Modifier Checker", "org.jenkins-ci", "access-modifier-checker", null, "https://github.com/jenkinsci/lib-access-modifier"),
+    new Artifact("Access Modifier Suppressions", "org.jenkins-ci", "access-modifier-suppressions", null, "https://github.com/jenkinsci/lib-access-modifier"),
+    new Artifact("Mock JavaMail", "org.jvnet.mock-javamail", "mock-javamail", null, "https://github.com/jenkinsci/lib-mock-javamail"),
+    new Artifact("Support Log Formatter", "io.jenkins.lib", "support-log-formatter", null, "https://github.com/jenkinsci/lib-support-log-formatter"),
+    new Artifact("Symbol Annotation", "org.jenkins-ci", "symbol-annotation", null, "https://github.com/jenkinsci/lib-symbol-annotation")
 ))
 
 
